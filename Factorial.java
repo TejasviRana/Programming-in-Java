3df871f28cbaaee08d5335224db9b6138c068851
TejasviRana <149259608+TejasviRana@users.noreply.github.com>
2025-03-06 11:55:59 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Factorial {
-    /**
-     * Prints the factorial of the given number to the console.
-     * @param args the command line arguments, not used
-     */
+    // Method to calculate factorial
+    public static long factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        long fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
     public static void main(String[] args) {
-        int num = 5, fact = 1;
-        for (int i = 1; i <= num; i++) fact *= i;
-        System.out.println("Factorial: " + fact);
+        Scanner scanner = new Scanner(System.in);
+        
+        // Taking input
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        // Calculating factorial
+        long result = factorial(num);
+        
+        // Displaying result
+        System.out.println("Factorial of " + num + " is: " + result);
+        
+        scanner.close();
     }
 }
+
 
