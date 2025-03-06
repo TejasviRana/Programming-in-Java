747aa7ef690b937ba3fc075f397cfe7d21476fcf
TejasviRana <149259608+TejasviRana@users.noreply.github.com>
2025-03-06 11:38:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,5 @@
 public class HelloWorld {
-/**
- * The main method that serves as the entry point for the program.
- * It prints "Hello, World!" to the standard output.
- * @param args command line arguments, not used in this program
- */
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
