b967ad78c9764086f5dc7ccffacc8912c61fd3bf
TejasviRana <149259608+TejasviRana@users.noreply.github.com>
2025-03-06 11:47:16 +0530

    Update AnagramCheck.java

--- AnagramCheck.java
+++ AnagramCheck.java
@@ -1,15 +1,29 @@
-package Part_3;
-
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 public class AnagramCheck {
-    /**
-     * Checks if two strings are anagrams of each other.
-     * @param args command line arguments, not used
-     */
-    public static void main(String[] args) {
-        char[] s1 = "Silent".toLowerCase().toCharArray();
-        char[] s2 = "Listen".toLowerCase().toCharArray();
-        Arrays.sort(s1); Arrays.sort(s2);
-        System.out.println(Arrays.equals(s1, s2) ? "Anagram" : "Not Anagram");
-    }
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter the string 1:");
+		String Str1 = sc.next();
+		System.out.print("enter the string 2:");
+		String Str2 = sc.next();
+		Str1= Str1.toLowerCase();
+		Str2= Str2.toLowerCase();
+		if (Str1.length()==Str2.length()) {
+			char[] a1= Str1.toCharArray();
+			char[] a2= Str2.toCharArray();
+			Arrays.sort(a1);
+			Arrays.sort(a2);
+			boolean result = Arrays.equals(a1,a2);
+			if (result) {
+				System.out.println(Str1+"and"+Str2+" are anagram");
+			}else {
+				System.out.println(Str1+"and"+Str2+"are not anagram");
+			}
+			
+			}
+		sc.close();
+	}
+
 }
