d5cac4db898590409fa977300b3fda55205875c5
TejasviRana <149259608+TejasviRana@users.noreply.github.com>
2025-03-06 12:33:28 +0530

    Update RemoveLeadingZeros.java

--- RemoveLeadingZeros.java
+++ RemoveLeadingZeros.java
@@ -1,17 +1,23 @@
 package Part_3;
 
-public class RemoveLeadingZeros {
-    /**
-     * The main method demonstrates removing leading zeros from a string.
-     * It initializes a string with leading zeros and prints the result
-     * after removing the leading zeros using regular expression.
-     *
-     * @param args command line arguments
-     */
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        String str = "00000123569";
-        System.out.println(str.replaceFirst("^0+", ""));
-    }
+public class Remove_Leading_Zeros {
+
+	public static void main(String[] args) {
+
+		String str = "00002347";
+		String S= "";
+		for (int i =0;i<str.length();i++ ) {
+			if (str.charAt(i)=='0') {
+				continue;
+				
+			}else {
+				S= str.substring(i);
+				break;
+			}
+		}
+		System.out.println(S);
+	}
+
 }
-
