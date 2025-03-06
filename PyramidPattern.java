7340924fd69ed1f4b17cffd2b4ed279859958197
TejasviRana <149259608+TejasviRana@users.noreply.github.com>
2025-03-06 12:12:29 +0530

    Update PyramidPattern.java

--- PyramidPattern.java
+++ PyramidPattern.java
@@ -1,18 +1,23 @@
-public class PyramidPattern {
-        /**
-         * Prints a pyramid pattern of stars of height n. The pattern for n = 4 is
-         *    *
-         *   ***
-         *  *****
-         * *******
-         * @param args command line arguments, not used
-         */
-    public static void main(String[] args) {
-        int n = 4;
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j < n; j++) System.out.print(" ");
-            for (int j = 1; j <= 2*i-1; j++) System.out.print("*");
-            System.out.println();
-        }
-    }
+import java.util.Scanner;
+public class Pyramid_Pattern {
+	public static void main(String[] args) {
+		System.out.println("The pyramid pattern");
+		int n;
+		Scanner sc= new Scanner(System.in);
+		System.out.print("enter the length of pyramid : ");
+		n=sc.nextInt();
+		for (int i=1 ; i<=n; i++) {
+			for (int j=1; j<=n-i;j++) {
+				System.out.print(" ");
+				
+			}
+			for (int k=1; k<=i*2-1;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+		sc.close();
+	}
+
 }
